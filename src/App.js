9e8ca1940394fd3678ffb1f9801d03043cff241c
dcameronnguyen <dcameron.nguyen@gmail.com>
2020-12-08 18:27:10 -0500

    setup signup page

--- src/App.js
+++ src/App.js
@@ -5,12 +5,26 @@ import Dashboard from './Pages/Dashboard';
 import HomePage from './Pages/HomePage';
 import LoginPage from './Pages/LoginPage';
 import SignupPage from './Pages/SignupPage';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      
+      <Switch>
+        <Route exact path="/" render={ props =>
+          <HomePage />
+        } />
+        <Route exact path="/dashboard" render={ props =>
+          <Dashboard />
+        } />
+        <Route exact path="/login" render={ props =>
+          <LoginPage />
+        } />
+        <Route exact path="/signup" render={ props =>
+          <SignupPage />
+        } />
+      </Switch>      
       <Footer />
     </div>
   );

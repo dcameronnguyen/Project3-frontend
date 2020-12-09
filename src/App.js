1474ecbbbf19132c0494784e8c2b61a743c91cc9
dcameronnguyen <dcameron.nguyen@gmail.com>
2020-12-09 10:55:31 -0500

    setup signup page

--- src/App.js
+++ src/App.js
@@ -1,33 +1,53 @@
-import './App.css';
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
-import Dashboard from './Pages/Dashboard';
-import HomePage from './Pages/HomePage';
-import LoginPage from './Pages/LoginPage';
-import SignupPage from './Pages/SignupPage';
-import { Route, Switch } from 'react-router-dom';
+import { useState } from "react";
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Dashboard from "./Pages/Dashboard";
+import HomePage from "./Pages/HomePage";
+import LoginPage from "./Pages/LoginPage";
+import SignupPage from "./Pages/SignupPage";
+import { Route, Switch, withRouter } from "react-router-dom";
+import { getUser } from "./services/userService";
 
-function App() {
+function App(props) {
+  // component state
+  const [userState, setUserState] = useState({
+    user: getUser(),
+  });
+
+  // helper functions
+
+  function handleSignUpOrLogin() {
+    // place user into state using setter function
+    setUserState({ user: getUser() });
+    // programatically route user to dashboard
+    props.history.push("/dashboard");
+  }
+
   return (
     <div className="App">
       <Header />
       <Switch>
-        <Route exact path="/" render={ props =>
-          <HomePage />
-        } />
-        <Route exact path="/dashboard" render={ props =>
-          <Dashboard />
-        } />
-        <Route exact path="/login" render={ props =>
-          <LoginPage />
-        } />
-        <Route exact path="/signup" render={ props =>
-          <SignupPage />
-        } />
-      </Switch>      
+        <Route exact path="/" render={(props) => <HomePage />} />
+        <Route exact path="/dashboard" render={(props) => <Dashboard />} />
+        <Route
+          exact
+          path="/login"
+          render={(props) => (
+            <LoginPage handleSignUpOrLogin={handleSignUpOrLogin} />
+          )}
+        />
+        <Route
+          exact
+          path="/signup"
+          render={(props) => (
+            <SignupPage handleSignUpOrLogin={handleSignUpOrLogin} />
+          )}
+        />
+      </Switch>
       <Footer />
     </div>
   );
 }
 
-export default App;
+export default withRouter(App);
--- src/services/tokenService.js
+++ src/services/tokenService.js
@@ -1,27 +1,34 @@
 function setToken(token) {
-    if(token) {
-      localStorage.setItem('token', token);
-    } else {
-      localStorage.removeItem('token');
+  if (token) {
+    localStorage.setItem("token", token);
+  } else {
+    localStorage.removeItem("token");
+  }
+}
+
+function removeToken() {
+  localStorage.removeItem("token");
+}
+
+function getToken() {
+  let token = localStorage.getItem("token");
+  // check if we even have the token
+  if (token) {
+    // extract the payload
+    const payload = JSON.parse(atob(token.split(".")[1]));
+    // check the expiration date
+    if (payload.exp < Date.now() / 1000) {
+      localStorage.removeItem("token");
+      token = null;
     }
   }
-  
-  function removeToken() {
-    localStorage.removeItem('token');
-  }
-  
-  function getToken() {
-  
-  }
-  
-  function getUserFromToken() {
-  
-  }
-  
-  
-  export {
-    setToken,
-    removeToken,
-    getToken,
-    getUserFromToken
-  }
+  // if all good return the token to next function
+  return token;
+}
+
+function getUserFromToken() {
+  const token = getToken();
+  return token ? JSON.parse(atob(token.split(".")[1])).user : null;
+}
+
+export { setToken, removeToken, getToken, getUserFromToken };

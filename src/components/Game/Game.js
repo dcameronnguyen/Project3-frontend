64ccae2c641140c16d5657e10aa71f1c1e4c125f
dcameronnguyen <dcameron.nguyen@gmail.com>
2020-12-14 17:34:00 -0500

    got api to display

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,9 +2,17 @@ import styles from "./Game.module.css";
 
 export default function Game(props) {
   return (
-    <div>
-      <h2>{props.game.name}</h2>
-      <p>{props.game.playtime}</p>
+    <div styles={styles.Poster}>
+      <h2>
+        <strong>{props.game.name}</strong>
+      </h2>
+      <p>Released Date: {props.game.released}</p>
+      <p>Rating: {props.game.rating}</p>
+      <p>Rated by: {props.game.ratings_count} users</p>
+      <p>Metacritic Score: {props.game.metacritic}</p>
+      <a href={props.game.background_image} target="_blank">
+        Click for Image
+      </a>
     </div>
   );
 }

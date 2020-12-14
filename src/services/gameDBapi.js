cdbcafb18a93bde3bc454171ac65613c38462f6f
dcameronnguyen <dcameron.nguyen@gmail.com>
2020-12-14 17:33:03 -0500

    got api to work

--- src/services/gameDBapi.js
+++ src/services/gameDBapi.js
@@ -1,5 +1,22 @@
-const BASE_URL = "https://api.rawg.io/api/games";
+require("dotenv").config();
+
+const key = process.env.API_KEY;
+
+const BASE_URL = `https://api.rawg.io/api/games?key=c12efb01df64453e9e6ee4e03d41f54d
+`;
+
+const SEARCH_URL = `https://api.rawg.io/api/games?key=c12efb01df64453e9e6ee4e03d41f54d
+&search=`;
 
 export function getGames() {
   return fetch(BASE_URL).then((res) => res.json());
+}
+
+export function getSearch(searchTerm) {
+  return fetch(SEARCH_URL + searchTerm, {
+    method: "GET",
+    headers: {
+      "Content-Type": "Application/Json",
+    },
+  }).then((res) => res.json());
 }

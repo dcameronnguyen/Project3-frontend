565f21f279e685c7087ec966ba31b43394761aaf
dcameronnguyen <dcameron.nguyen@gmail.com>
2020-12-14 19:47:31 -0500

    moved image

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -6,6 +6,7 @@ export default function Game(props) {
       <h2>
         <strong>{props.game.name}</strong>
       </h2>
+      <img src={props.game.background_image} alt="poster" className="Picture" />
       <p>Released Date: {props.game.released}</p>
       <p>Rating: {props.game.rating}/5</p>
       <p>Rated by: {props.game.ratings_count} users</p>
@@ -13,7 +14,6 @@ export default function Game(props) {
       <p>Average Playtime: {props.game.playtime} hrs</p>
       <p>Last Updated: {props.game.updated}</p>
       <br />
-      <img src={props.game.background_image} alt="poster" className="Picture" />
       <br />
       <br />
     </div>
